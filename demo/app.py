2776da4efa0b9f692a0b722aa536addc0bcfe13a
kolamor <kolamorev@mail.ru>
2018-12-03 00:53:59 +0300

    asyncpgsa and  bd yaml config

--- demo/app.py
+++ demo/app.py
@@ -1,7 +1,9 @@
 from aiohttp import web
 import jinja2
 import aiohttp_jinja2
+import asyncpgsa
 from .routes import setup_routes
+
 
 
 async def create_app(config:dict):
@@ -12,4 +14,14 @@ async def create_app(config:dict):
 		loader = jinja2.PackageLoader('demo', 'templates'),
 		)
 	setup_routes(app)
+	app.on_startup.append(on_start)
+	app.on_cleanup.append(on_shutdown)
+
 	return app
+
+async def on_start(app):
+	config = app['config']
+	app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
+
+async def on_shutdown(app):
+	await app['db'].close()
