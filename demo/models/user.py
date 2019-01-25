fdc051b688827055f5b83651f5b4eef664f97546
kolamor <kolamorev@mail.ru>
2019-01-25 15:56:55 +0300

    remove fold

--- demo/models/user.py
+++ demo/models/user.py
@@ -4,24 +4,6 @@ from .. import db
 
 class User:
 
-	# @staticmethod
-	# async def get_user_from_id(news, request):
-	# 	user_dict = {}
-	# 	for new in news:
-	# 		user = await User.user_name(new['id'], request)
-	# 		user = {user['id'] : user['login']}
-	# 		user_dict.update(user)
-	# 	return user_dict	
-
-
-	# @staticmethod
-	# async def user_name(user_id, request):
-
-	# 	async with request.app['db'].acquire() as conn:
-	# 		query = select([db.user_d.c.login, db.user_d.c.id ]).where(db.user_d.c.id == user_id)
-	# 		user = await conn.fetchrow(query)
-	# 	return user
-
 	@classmethod
 	async def create(cls, request, **kwargs):
 		self = cls()
--- demo/views/admin.py
+++ demo/views/admin.py
@@ -159,32 +159,6 @@ async def admin_privilege_valid(request):
 		return user
 
 
-
-# async def edit_user_db(request, name):
-# 	data = await request.post()
-# 	data = dict(data)
-# 	async with request.app['db'].acquire() as conn:
-# 		query = select([db.user_d]).where(db.user_d.c.id == int(data['id']))
-# 		user = await conn.fetchrow(query)
-# 		try:
-# 			val_admin_privilege = data.pop('admin_privilege')
-# 			query = update(db.user_d).where(db.user_d.c.id == user['id']).values({
-# 											'admin_privilege' : bool(val_admin_privilege)})
-# 		except KeyError as e:
-# 			query = update(db.user_d).where(db.user_d.c.id == user['id']).values({
-# 											'admin_privilege' : False })
-# 		await conn.execute(query)		
-
-# 		if '' not in data.values():
-# 			query = update(db.user_d).where(db.user_d.c.id == user['id']).values({
-# 					'login'    : data['login'],
-# 					'email'    : data['email'],
-# 					'password' : data['password']})
-# 			await conn.execute(query)
-# 	return user['id']
-
-
-
 class CreateNews(aiohttp.web.View):
 	
 	@template('/admin/create_news.html')
